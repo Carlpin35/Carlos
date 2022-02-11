c471a9007b71babb573b68929ec711b36e3049bc
Carlpin35 <carlpin800@gmail.com>
2022-02-11 10:14:33 -0300

    first commit

---
+++ constants/data.js
@@ -0,0 +1,72 @@
+const userData = {
+  githubUsername: "Carlpin35",
+  name: "Carlos Pineda",
+  designation: "Front-End Developer",
+  avatarUrl: "/avatar.png",
+  email: "Carlpin800@gmail.com",
+  phone: "+54 1127910428",
+  address: "Buenos Aires, Argentina.",
+  projects: [
+    {
+      title: "Linkedin clone",
+      link: "https://linkedin-clone-9sjkmsdnp-carlpin35.vercel.app/",
+      imgUrl: "/linkedin.png",
+    },
+    {
+      title: "Instagram clone",
+      link: "https://instagram-clone-dun-ten.vercel.app/",
+      imgUrl: "/instagram.png",
+    },
+    {
+      title: "Hulu clone",
+      link: "https://hlu-clon.vercel.app/",
+      imgUrl: "/hulu.png",
+    },
+    {
+      title: "Google clone",
+      link: "https://ggle-clon.vercel.app/",
+      imgUrl: "/google.png",
+    },
+    {
+      title: "Twitter clone",
+      link: "https://twitter-clon-two.vercel.app/",
+      imgUrl: "/twitter.png",
+    },
+    {
+      title: "Amazon clone",
+      link: "https://amazon-clone-lime-ten.vercel.app/",
+      imgUrl: "/amazon.png",
+    },
+    {
+      title: "Spotify clone",
+      link: "https://spt-clone.vercel.app/",
+      imgUrl: "/spotify.png",
+    },
+    /*{
+      title: "Whatsapp clone",
+      link: "https://devmedium.vercel.app",
+      imgUrl: "/whatsapp.png",
+    },*/
+  ],
+  about: {
+    title:
+      "Hi there! I am Carlos! am a Buenos Aires based front end Developer",
+    description: [
+      `de`,
+    ],
+    currentProject: " web 3.0 related project",
+    currentProjectUrl: "hcom",
+  },
+ 
+  resumeUrl:
+    "https://drive.google.com/file/d/1xmE3BOmgM7TAOOgVp36xQIQvYDntDYoo/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://instagram.com/",
+    twitter: "https://twitter.com/",
+    linkedin: "https://www.linkedin.com/in/carlos-pineda-aa6b5322a/",
+    github: "https://github.com/Carlpin35",
+    facebook: "https://facebook.com/",
+  },
+};
+
+export default userData;
